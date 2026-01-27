3fd1f15b9f11651401aaa7a63c62af31f29bbe63
Jordan Krueger <jordankrueger@Jordans-Mac-mini.local>
2026-01-27 11:11:36 -0500

    Add Google Analytics tracking (G-9BX8DMYS12)

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+// Site-wide configuration
+// This file is version controlled and contains deploy-time settings
+
+module.exports = {
+  googleAnalyticsId: 'G-9BX8DMYS12',
+  siteUrl: 'https://highspeedrail.tv',
+  siteName: 'HighSpeedRail.TV'
+};
